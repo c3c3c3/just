af978382ff2c3c747bd47549a730da90598c4389
c3c3c3 <32392006+c3c3c3@users.noreply.github.com>
2017-09-29 15:56:29 +0200

    Add files via upload

---
+++ 5.8.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+ 
+void Sort(int *, int); 
+ 
+int main(int argc, char* argv[])
+{
+   
+    cout << "Razmer: "<<"\n";;
+    int s; 
+    cin >> s;
+ cout<<"\n";
+    int *k = new int [s]; 
+    for (int i = 0; i < s; i++)
+    {
+        k[i] = rand() % 100; 
+        cout << k[i] <<" "<<"\n";; 
+    }
+    
+ 
+    Sort(k, s); 
+ cout<<"Rezul'tat: ";
+    for (int j = 0; j < s; j++)
+    {
+        cout<<k[j]<<"  "; 
+    }
+   
+    system("pause");
+    return 0;
+}
+ 
+void Sort(int *g, int l) 
+{
+    int t, 
+        f; 
+    for (int i = 1; i < l; i++)
+    {
+        t = g[i]; 
+        f = i-1; 
+        while(f >= 0 && g[f] > t) 
+        {
+            g[f+ 1] = g[f]; 
+            g[f] = t;
+            f--;
+        }}}
